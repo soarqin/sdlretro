dfb26a321f647feae5c751554095d4c3dd458562
Soar Qin <soarchin@gmail.com>
2020-08-17 02:01:28 +0800

    use SDL 2.0 functions for audio output

--- src/drivers/sdl2/sdl2_audio.cpp
+++ src/drivers/sdl2/sdl2_audio.cpp
@@ -12,26 +12,27 @@ void sdl2_audio::audio_callback(void *userdata, uint8_t *stream, int len) {
 bool sdl2_audio::open(unsigned buffer_size) {
     if (!SDL_WasInit(SDL_INIT_AUDIO))
         SDL_InitSubSystem(SDL_INIT_AUDIO);
-    SDL_AudioSpec spec = {};
+    SDL_AudioSpec spec = {}, obtained = {};
     spec.callback = audio_callback;
     spec.freq = static_cast<int>(output_sample_rate);
     spec.format = AUDIO_S16SYS;
     spec.channels = mono_audio ? 1 : 2;
     spec.samples = buffer_size;
     spec.userdata = this;
-    if (SDL_OpenAudio(&spec, nullptr) != 0) return false;
+    if ((device_id = SDL_OpenAudioDevice(nullptr, 0, &spec, &obtained, SDL_AUDIO_ALLOW_SAMPLES_CHANGE | SDL_AUDIO_ALLOW_FREQUENCY_CHANGE)) < 2) return false;
+    output_sample_rate = obtained.freq;
 
-    SDL_PauseAudio(0);
+    SDL_PauseAudioDevice(device_id, 0);
     return true;
 }
 
 void sdl2_audio::close() {
     output_sample_rate = 0;
-    SDL_CloseAudio();
+    SDL_CloseAudioDevice(device_id);
 }
 
 void sdl2_audio::pause(bool b) {
-    SDL_PauseAudio(b);
+    SDL_PauseAudioDevice(device_id, b);
 }
 
 }
