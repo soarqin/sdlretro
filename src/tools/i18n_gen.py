351df42ca6d74e95ad2dc996768b341dbaac3dc1
Soar Qin <soarchin@gmail.com>
2020-09-13 11:14:50 +0800

    finished tool for language file gen/update, as well as zh-CN language

--- src/tools/i18n_gen.py
+++ src/tools/i18n_gen.py
@@ -3,7 +3,6 @@
 import re
 import glob
 import os
-import sys
 import codecs
 import json
 from collections import OrderedDict
@@ -22,11 +21,23 @@ if __name__ == '__main__':
                 if os.path.splitext(y)[1] in ['.c', '.cpp']:
                     text_dict.update(OrderedDict.fromkeys(get_translation_text_list(y)))
 
+    for x in os.walk('../../lang'):
+        for y in glob.glob(os.path.join(x[0], '*.json')):
+            tr_dict = text_dict.copy()
+            depr_dict = OrderedDict()
+            with codecs.open(y, 'r', 'utf-8') as f:
+                org_dict = OrderedDict(json.load(f))
+                for k, v in org_dict.items():
+                    if (k in tr_dict):
+                        tr_dict[k] = v
+                    elif k[-12:] == '(deprecated)':
+                        depr_dict[k] = v
+                    else:
+                        depr_dict['__' + k + '__(deprecated)'] = v
+            tr_dict.update(depr_dict)
+            with codecs.open(y, 'w', 'utf-8') as f:
+                json.dump(tr_dict, f, indent=4, ensure_ascii=False)
     for v in text_dict.keys():
         text_dict[v] = v
-    if len(sys.argv) < 2:
-        filename = 'en-US'
-    else:
-        filename = sys.argv[1]
-    with open('../../lang/' + filename + '.json', 'w') as f:
-        json.dump(text_dict, f, indent=4)
+    with codecs.open('../../lang/en-US.json', 'w', 'utf-8') as f:
+        json.dump(text_dict, f, indent=4, ensure_ascii=False)
